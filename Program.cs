cdf879db691ffdea5f52b958d61dd99080685683
AnthonyFinney <anthonyrockfinnie0@gmail.com>
2025-02-26 20:39:04 +0600

    commit

--- Program.cs
+++ Program.cs
@@ -31,7 +31,8 @@ if (!app.Environment.IsDevelopment()) {
 }
 
 var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
-app.Urls.Add($"http://0.0.0.0:{port}");
+var host = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Development" ? "localhost" : "0.0.0.0";
+app.Urls.Add($"http://{host}:{port}");
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
